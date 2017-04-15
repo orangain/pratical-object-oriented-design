5764b99547a8973f0f784049ad0aabb69d945570
orangain <orangain@gmail.com>
2017-04-15 22:34:34 +0900

    p164

--- ch06.rb
+++ ch06.rb
@@ -1,10 +1,15 @@
 class Bicycle
-  attr_reader :size
+  attr_reader :size, :chain, :tire_size
 
   def initialize(args={})
     @size = args[:size]
+    @chain = args[:chain] || default_chain
+    @tire_size = args[:tire_size] || default_tire_size
   end
 
+  def default_chain
+    '10-speed'
+  end
 end
 
 class RoadBike < Bicycle
@@ -13,6 +18,10 @@ class RoadBike < Bicycle
   def initialize(args)
     @tape_color = args[:tape_color]
     super(args)
+  end
+
+  def default_tire_size
+    '23'
   end
 
   def spares
@@ -34,6 +43,10 @@ class MountainBike < Bicycle
     super(args)
   end
 
+  def default_tire_size
+    '2.1'
+  end
+
   def spares
     super.merge(front_shock: front_shock, rear_shock: rear_shock)
   end
@@ -43,12 +56,13 @@ road_bike = RoadBike.new(
   size: 'M',
   tape_color: 'red')
 
-puts road_bike.size
+puts road_bike.tire_size
+puts road_bike.chain
 
 mountain_bike = MountainBike.new(
   size: 'S',
   front_shock: 'Manitou',
   rear_shock: 'Fox')
 
-puts mountain_bike.size
-puts mountain_bike.spares
+puts mountain_bike.tire_size
+puts mountain_bike.chain
