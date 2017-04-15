b1d5926e38c9648ee3c6e0807ac8ecac21c97cf0
orangain <orangain@gmail.com>
2017-04-15 22:19:17 +0900

    p151

--- ch06.rb
+++ ch06.rb
@@ -16,9 +16,24 @@ class Bicycle
 
 end
 
-bike = Bicycle.new(
-  size: 'M',
-  tape_color: 'red')
+class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
+    super(args)
+  end
+
+  def spares
+    super.merge(rear_shock: rear_shock)
+  end
+end
+
+bike = MountainBike.new(
+  size: 'S',
+  front_shock: 'Manitou',
+  rear_shock: 'Fox')
 
 puts bike.size
 puts bike.spares
