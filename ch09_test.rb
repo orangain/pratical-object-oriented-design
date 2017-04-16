a1a6eb3dafb5f70e4249f7b91e92288388508306
orangain <orangain@gmail.com>
2017-04-16 15:27:56 +0900

    p262

--- ch09_test.rb
+++ ch09_test.rb
@@ -39,6 +39,14 @@ end
 
 class WheelTest < Test::Unit::TestCase
 
+  def setup
+    @wheel = Wheel.new(26, 1.5)
+  end
+
+  def test_implements_the_diameterizable_interface
+    assert_respond_to(@wheel, :diameter)
+  end
+
   def test_calculates_diameter
     wheel = Wheel.new(26, 1.5)
     assert_in_delta(29, wheel.diameter, 0.01)
